58f2e6fdc56f1b9012267083a749c5dacbef1a06
CharSnake <unconfigured@null.spigotmc.org>
2018-02-10 09:39:56 -0400

    Stage 3

--- app/src/main/java/com/example/user/csci3130assignment2/MainActivity.java
+++ app/src/main/java/com/example/user/csci3130assignment2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.csci3130assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submit(android.view.View v) {
+
+        //Get the value in the password selection and validate it
+        TextView pass = (TextView)findViewById(R.id.password);
+        Validator val = new Validator();
+        int tests = val.validate(pass.getText().toString());
+
+        //See if the password is valid and display if so.
+        TextView check = (TextView)findViewById(R.id.textView1);
+        if (tests == 5)
+            check.setText("Strong password.");
+        else
+            check.setText("Password not strong enough.");
+
     }
 }
--- app/src/main/java/com/example/user/csci3130assignment2/Validator.java
+++ app/src/main/java/com/example/user/csci3130assignment2/Validator.java
@@ -14,22 +14,34 @@ public class Validator {
         tests = 0;
 
         //Test 1, string does not equal "password"
+        //Ex: "PassWorD" and any other case combinations will fail, but "Password5" will succeed
         if (!pass.equalsIgnoreCase("password"))
+           tests += 1;
+
+        //Alternate Test 1, string does not CONTAIN "password"
+        //Ex: "24PassWorDqz" will fail
+        /*
+        if (!pass.toLowerCase().contains("password"))
             tests += 1;
+        */
 
         //Test 2, string is at least 8 characters long
+        //Ex: "vV34567" will fail, "vV345678" will succeed
         if (pass.length() >= 8)
             tests += 1;
 
         //Test 3, string contains at least one number
+        //Ex: "Abcdefgh" will fail, "Abcdefg8" will succeed
         if (pass.matches(".*\\d.*"))
             tests += 1;
 
         //Test 4, string contains at least one upper case AND one lower case
+        //Ex: "abcdefg8" will fail, "Abcdefg8" will succeed
         if (pass.matches(".*[a-z].*") && pass.matches(".*[A-Z].*"))
             tests += 1;
 
         //Test 5, string does not contain three of the same character in a row
+        //Ex: "HellloW0rld" will fail, "HelloW0rld" will succeed.
         if (!pass.matches(".*(.)\\1{2}.*"))
             tests += 1;
 
